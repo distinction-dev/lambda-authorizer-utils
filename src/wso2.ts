ed64415f89a9cf9f9fa64f390a37fc7d24f1fba8
KillDozerX2 <himanshupant127@gmail.com>
2023-01-09 18:42:55 +0530

    feat: :sparkles: added implementation for wso2 identity auth server

---
+++ src/wso2.ts
@@ -0,0 +1,14 @@
+import { BaseClaims, Verifier } from "@/jwt";
+
+export class CognitoVerifier<T extends BaseClaims> extends Verifier<T> {
+  /**
+   * BaseUrl for issuer host
+   */
+  private issuerHost: string;
+
+  constructor(params: { issuerHost: string; disableCaching: boolean }) {
+    if (!params.issuerHost) throw Error("Issuer host param is required");
+    super(`${params.issuerHost}/oauth2/jwks`, params.disableCaching);
+    this.issuerHost = params.issuerHost;
+  }
+}
