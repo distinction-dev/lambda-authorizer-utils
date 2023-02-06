2d9893463b01b4d843490c14c43c11a5819419c3
Tejas Dhamecha <tejasdhamecha231@gmail.com>
2023-02-06 21:05:22 +0530

    test: :bug: add unit test for path variables
    add unit test to replace path variables with "*"

---
+++ test/route-arn.test.ts
@@ -0,0 +1,42 @@
+import "mocha";
+import { buildRouteArn } from "../src";
+import { expect } from "chai";
+
+const region = "us-west-2";
+const awsAccountId = "102413378525";
+const apiId = "np6plfykai";
+const stage = "dev";
+const method = "GET";
+
+describe("Route ARN Path Regex Test Suite", () => {
+  it("Will replace path variables with '*'", () => {
+    const path = "/variable/{id}";
+
+    const result = buildRouteArn(
+      region,
+      awsAccountId,
+      apiId,
+      stage,
+      method,
+      path
+    );
+    expect(result).to.equal(
+      `arn:aws:execute-api:${region}:${awsAccountId}:${apiId}/${stage}/${method}/variable/*`
+    );
+  });
+  it("Will replace multiple path variables with '*'", () => {
+    const path = "/variable/{id}/hello/{name}";
+
+    const result = buildRouteArn(
+      region,
+      awsAccountId,
+      apiId,
+      stage,
+      method,
+      path
+    );
+    expect(result).to.equal(
+      `arn:aws:execute-api:${region}:${awsAccountId}:${apiId}/${stage}/${method}/variable/*/hello/*`
+    );
+  });
+});
