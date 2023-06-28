29d5600a303e8691ddbf2e52b00cf3a5ff405820
Tejas Dhamecha <tejasdhamecha231@gmail.com>
2023-06-28 11:29:54 +0530

    refactor: :recycle: move basepath to a variable

--- test/api.test.ts
+++ test/api.test.ts
@@ -2,6 +2,7 @@ import "mocha";
 import axios, { AxiosError } from "axios";
 import { create } from "njwt";
 import { expect, assert } from "chai";
+const basePath = "https://xyym7mxtoa.execute-api.us-east-1.amazonaws.com/dev";
 
 describe("API Test Suite", () => {
   const superUserToken = create(
@@ -17,48 +18,36 @@ describe("API Test Suite", () => {
     "test-key"
   ).compact();
   it("Will allow superuser", async () => {
-    const response = await axios.get(
-      "https://xyym7mxtoa.execute-api.us-east-1.amazonaws.com/dev/hello",
-      {
-        headers: {
-          Authorization: superUserToken,
-        },
-      }
-    );
+    const response = await axios.get(`${basePath}/hello`, {
+      headers: {
+        Authorization: superUserToken,
+      },
+    });
     expect(response.status).to.equal(200);
   });
   it("Will allow /hello to admin", async () => {
-    const response = await axios.get(
-      "https://xyym7mxtoa.execute-api.us-east-1.amazonaws.com/dev/hello",
-      {
-        headers: {
-          Authorization: adminToken,
-        },
-      }
-    );
+    const response = await axios.get(`${basePath}/hello`, {
+      headers: {
+        Authorization: adminToken,
+      },
+    });
     expect(response.status).to.equal(200);
   });
   it("Will allow /variable/{id} to admin", async () => {
-    const response = await axios.get(
-      "https://xyym7mxtoa.execute-api.us-east-1.amazonaws.com/dev/variable/420",
-      {
-        headers: {
-          Authorization: adminToken,
-        },
-      }
-    );
+    const response = await axios.get(`${basePath}/variable/420`, {
+      headers: {
+        Authorization: adminToken,
+      },
+    });
     expect(response.status).to.equal(200);
   });
   it("Will not allow /hello2 to admin", async () => {
     try {
-      await axios.get(
-        "https://xyym7mxtoa.execute-api.us-east-1.amazonaws.com/dev/hello2",
-        {
-          headers: {
-            Authorization: adminToken,
-          },
-        }
-      );
+      await axios.get(`${basePath}/hello2`, {
+        headers: {
+          Authorization: adminToken,
+        },
+      });
     } catch (err) {
       const axiosError = err as AxiosError;
       assert.isNotNull(axiosError.response);
